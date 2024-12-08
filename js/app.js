8ff51505f5023ad7b5f75c98c387b6aebbc4c3db
Ayush Kumar Nath <n.ayushknath@gmail.com>
2024-12-08 19:11:57 +0530

    Shows fetching screen while fetching weather data

--- js/app.js
+++ js/app.js
@@ -9,7 +9,9 @@ async function handleSubmit(e) {
   const validationPattern = /^[a-zA-Z0-9\s,'-]{2,}$/;
   if (inputValue && validationPattern.test(inputValue)) {
     try {
+      weatherDisplaySection.classList.add("fetching");
       const response = await getWeather(inputValue);
+      weatherDisplaySection.classList.remove("fetching");
       response.name =
         inputValue.slice(0, 1).toUpperCase() + inputValue.slice(1);
       displayWeather(response);
@@ -22,5 +24,8 @@ async function handleSubmit(e) {
 }
 
 const locationForm = document.querySelector("[data-location-form]");
+const weatherDisplaySection = document.querySelector(
+  "[data-weather-display-section]"
+);
 locationForm.addEventListener("submit", handleSubmit);
 updatePinnedUI();
